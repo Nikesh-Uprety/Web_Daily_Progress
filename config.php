27596f311613b93f7c17c26d42990394cb2b7a8d
Nikesh Uprety <109953698+Nikesh-Uprety@users.noreply.github.com>
2022-12-18 09:37:24 +0545

    Login page user and admin with php

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+$conn = mysqli_connect('localhost','nikesh','niku','user_data');
+
+?>
---
+++ login_form.php
@@ -0,0 +1,124 @@
+<?php
+//@include 'config.php'; 
+//
+//// Start the session
+//session_start();
+//if(isset($_POST['submit'])){
+//   // $name = mysqli_real_escape_string($conn, $_POST['name']);
+//   $email = mysqli_real_escape_string($conn, $_POST['email']);
+//   $pass = ($_POST['password']);
+//
+//   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+//
+//   $result = mysqli_query($conn, $select);
+//
+//   if(mysqli_num_rows($result) > 0){
+//
+//      $row = mysqli_fetch_array($result);
+//
+//      if($row['user_type'] == 'admin'){
+//
+//         $_SESSION['admin_name'] = $row['name'];
+//         header('location:admin_page.php');
+//
+//      }elseif($row['user_type'] == 'user'){
+//
+//         $_SESSION['user_name'] = $row['name'];
+//         header('location:user_page.php');
+//
+//      }
+//     
+//   }else{
+//      $error[] = 'incorrect email or password!';
+//   }
+//
+//};
+//
+
+@include 'config.php'; 
+
+// Start the session
+session_start();
+if(isset($_POST['submit'])){
+   // $name = mysqli_real_escape_string($conn, $_POST['name']);
+   $email = mysqli_real_escape_string($conn, $_POST['email']);
+   $pass = ($_POST['password']);
+
+   // Create a session ID that includes the email and password
+   $session_id = md5($email . $pass);
+
+   // Set the session ID
+   session_id($session_id);
+
+   $select = " SELECT * FROM user_form WHERE email = '$email' && password = '$pass' ";
+
+   $result = mysqli_query($conn, $select);
+
+   if(mysqli_num_rows($result) > 0){
+
+      $row = mysqli_fetch_array($result);
+
+      if($row['user_type'] == 'admin'){
+
+         $_SESSION['admin_name'] = $row['name'];
+         header('location:admin_page.php');
+
+      }elseif($row['user_type'] == 'user'){
+
+         $_SESSION['user_name'] = $row['name'];
+         header('location:user_page.php');
+
+      }
+     
+   }else{
+      $error[] = 'incorrect email or password!';
+   }
+
+};
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>login form</title>
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="form-container">
+
+   <form action="" method="post">
+      <h3>login now</h3>
+      <?php
+      if(isset($error)){
+         foreach($error as $error){
+            echo '<span class="error-msg">'.$error.'</span>';
+         };
+      };
+      ?>
+      <input type="email" name="email" required placeholder="enter your email">
+      <input type="password" name="password" required placeholder="enter your password">
+      <input type="submit" name="submit" value="login now" class="form-btn">
+      <p>don't have an account? <a href="register_form.php">register now</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
+<input type="submit" name="submit" value="login now" class="form-btn">
+      <p>don't have an account? <a href="register_form.php">register now</a></p>
+   </form>
+
+</div>
+
+</body>
+</html>
+
+
